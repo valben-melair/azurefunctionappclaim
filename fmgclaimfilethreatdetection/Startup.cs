d0c8f07cbd742bad61d98b0ccc6441342db1cd82
Valben Galon <valbengalon@hotmail.com>
2022-03-07 21:05:08 +1300

    Update startup compliant

--- fmgclaimfilethreatdetection/Startup.cs
+++ fmgclaimfilethreatdetection/Startup.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Net.Http;
+using System.Net.Security;
 
 [assembly: FunctionsStartup(typeof(FMG.ClaimFileUpload.Startup))]
 namespace FMG.ClaimFileUpload
@@ -14,8 +16,13 @@ namespace FMG.ClaimFileUpload
                     () => new HttpClientHandler()
                     {
                         ServerCertificateCustomValidationCallback =
-                                (sender, certificate, chain, errors) => { return true; }
-                    });
+                            (sender, certificate, chain, errors) =>
+                            {
+                                // local dev, just approve all certs
+                                if (Environment.GetEnvironmentVariable("IsDev") == "1") return true;
+                                return errors == SslPolicyErrors.None;
+                            }
+        });
         }
     }
 }
